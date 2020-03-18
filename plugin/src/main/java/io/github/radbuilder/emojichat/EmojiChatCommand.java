5789e4babce95b31e007cfe91ac9ffda60c2b354
James76931 <james76931@me.com>
2020-03-18 15:59:15 +0000

    Update EmojiChatCommand.java

--- plugin/src/main/java/io/github/radbuilder/emojichat/EmojiChatCommand.java
+++ plugin/src/main/java/io/github/radbuilder/emojichat/EmojiChatCommand.java
@@ -32,6 +32,7 @@ class EmojiChatCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
 		if (args.length < 1) {
 			sender.sendMessage(ChatColor.AQUA + "EmojiChat v1.8.3 by RadBuilder");
+			sender.sendMessage(ChatColor.GREEN + "Modified by James76931");
 			sender.sendMessage(ChatColor.AQUA + "Use " + ChatColor.GREEN + "/emojichat help" + ChatColor.AQUA + " for help.");
 			return true;
 		}
